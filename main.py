b953e4bb9ff06209f64e786b6e89b567bd57e462
Mary Brown <marycbrown2004@gmail.com>
2020-07-22 16:32:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+a = int(input("Type a number, 1-4: "))
+if a == 1:
+  print("Their teeth grow forever")
+if a == 2:
+  print("Their tails are hairless so that they can grip with them")
+if a == 3:
+  print("They can jump 13 inches high")
+if a == 4:
+  print("They can barely see")
+
+#its a mouse
